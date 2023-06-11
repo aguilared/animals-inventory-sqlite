41ef020c9bb7bd3f2b3e8d583980c91e1b7aef7b
Arquimedes I <aguiland06@gmail.com>
2023-06-11 10:21:47 -0400

    afixed type to let row 3

--- src/hooks/useVacas.ts
+++ src/hooks/useVacas.ts
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import getVacas from "../services/getVacas";
 
-export interface Ivaca {
+export interface IVaca {
   label: string;
   value: number;
 }
 
 export function useVacas() {
-  const [vacas, setVacas] = useState<Ivaca[] | []>([]);
+  const [vacas, setVacas] = useState<IVaca[] | []>([]);
   //console.log("useVacas", getVacas());
   useEffect(
     function () {
       let options = [{ value: 0, label: "Seleccione.." }];
       getVacas().then((vaca) => {
         //console.log("VACA", vaca);
-        vaca.forEach((option:any) => {
-          let row = {};
+        vaca.forEach((option: any) => {
+          let row = <IVaca>{};
           row.value = option.id;
           row.label = " " + option.id + " " + option.name;
           options.push(row);
