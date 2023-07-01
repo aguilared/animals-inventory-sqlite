23a93f95123cc88b87f31661047cf2c7bbcd11bd
Arquimedes I <aguiland06@gmail.com>
2023-07-01 07:50:20 -0400

    add logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const links = [
   { href: "/animals/", label: "List" },
@@ -11,21 +12,18 @@ const links = [
 const Navigation = () => (
   <nav className="flex rounded items-center justify-between flex-wrap bg-gray-700 p-2 py-1">
     <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <svg
-        className="fill-current h-8 w-8 mr-2"
-        width="54"
-        height="54"
-        viewBox="0 0 54 54"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-      </svg>
+      <Image
+        src={"/static/images/logo.jpg"}
+        width={70}
+        height={50}
+        alt=""
+        style={{ objectFit: "cover" }}
+      />
 
       <span className="sm:inline-block font-semibold text-xl tracking-tight">
-        Gonzalera Ranch - Animals Inventory
+        Gonzalera Ranch - Inventory
       </span>
     </div>
-   
 
     <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
       <div className="text-sm sm:flex-grow">
