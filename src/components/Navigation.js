2aa7a52706d2b471f2f3735064fcd39137d2bc8e
Arquimedes I <aguiland06@gmail.com>
2023-07-05 10:06:07 -0400

    add login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import useUser from "../hooks/useUser";
+import axios from "axios";
 
 const links = [
   { href: "/animals/", label: "List" },
@@ -9,43 +12,73 @@ const links = [
   return link;
 });
 
-const Navigation = () => (
-  <nav className="flex rounded items-center justify-between flex-wrap bg-gray-700 p-2 py-1">
-    <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <Image
-        src={"/static/images/logo.jpg"}
-        width={70}
-        height={50}
-        alt=""
-        style={{ objectFit: "cover" }}
-      />
+const Navigation = () => {
+  const router = useRouter();
+  const { isUser, loadUser, clearUser } = useUser(); //to Global
+  const logout = async () => {
+    console.error("HaciendoLogout");
+    try {
+      await axios.get("/api/auth/logout");
+      clearUser();
+      router.push("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-      <span className="sm:inline-block font-semibold text-xl tracking-tight">
-        Gonzalera Ranch - Inventory
-      </span>
-    </div>
+  return (
+    <nav className="flex rounded items-center justify-between flex-wrap bg-gray-700 p-2 py-1">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <Image
+          src={"/static/images/logo.jpg"}
+          width={70}
+          height={50}
+          alt=""
+          style={{ objectFit: "cover" }}
+        />
 
-    <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
-      <div className="text-sm sm:flex-grow">
-        <Link
-          href="/"
-          className="mt-4 sm:mt-0 text-white hover:text-white mr-4"
-        >
-          Home
-        </Link>
-
-        {links.map(({ key, href, label }) => (
-          <Link
-            key={key}
-            href={href}
-            className="mt-1 sm:mt-0 text-white hover:text-white mr-4"
-          >
-            {label}
-          </Link>
-        ))}
+        <span className="sm:inline-block font-semibold text-xl tracking-tight">
+          Gonzalera Ranch - Inventory
+        </span>
       </div>
-    </div>
-  </nav>
-);
+
+      <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
+        <div className="text-sm sm:flex-grow">
+          <Link
+            href="/"
+            className="mt-4 sm:mt-0 text-white hover:text-white mr-4"
+          >
+            Home
+          </Link>
+
+          {links.map(({ key, href, label }) => (
+            <Link
+              key={key}
+              href={href}
+              className="mt-1 sm:mt-0 text-white hover:text-white mr-4"
+            >
+              {label}
+            </Link>
+          ))}
+          {isUser ? (
+            <button
+              onClick={() => logout()}
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-gray mt-4 sm:mt-0"
+            >
+              LogOut
+            </button>
+          ) : (
+            <Link
+              href="/login/"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-gray mt-4 sm:mt-0"
+            >
+              Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
