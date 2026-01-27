1f2381e5065e2508a37b42b472739c64acdbb0a9
aguilared <arquimedesgui@gmail.com>
2026-01-27 17:13:11 -0400

    search bitacora tipoevent

---
+++ src/hooks/bitacora/useTypeEvents1.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import getTypeEvents from "@/services/bitacora/getTypeEvents";
+
+export function useTypeEvents1() {
+  const [typeEvents1, setTypeEvents1] = useState({});
+  //console.log("usetypeEvents1")
+  useEffect(
+    function () {
+      let options = [{ value: 0, label: "Seleccione" }];
+      getTypeEvents().then((typeEvent) => {
+        console.log("typeEventss", typeEvent);
+        typeEvent.forEach((option) => {
+          let row = {};
+          row.value = option.id;
+          row.label = " " + option.id + " " + option.description;
+          row.isDisabled = false;
+          options.push(row);
+        });
+        console.log("Options", options);
+        setTypeEvents1(options);
+      });
+    },
+    [setTypeEvents1]
+  );
+  return { typeEvents1 };
+}
