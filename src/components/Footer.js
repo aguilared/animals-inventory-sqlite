80d86a4807293b1b91e84dbf6b607d6886e79c4c
aguilared <arquimedesgui@gmail.com>
2026-01-27 19:36:12 -0400

    copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
       <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
-        © 2023 Copyright: Gonzalera Ranch,  Animals Inventory
+        © 2026 Copyright: Gonzalera Ranch,  Animals Inventory
       </div>
     </footer>
   );
